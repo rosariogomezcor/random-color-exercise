784255db0eb5f52f394c2ac03454ad79b66d3323
Rosario <rosariogomez639@gmail.com>
2022-09-17 19:45:56 -0300

    add logic

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("connected"); 
+const button = document.querySelector('button'); 
+const h1 = document.querySelector('h1'); 
+button.addEventListener('click', () => {
+    const newColor = randomColor(); 
+    document.body.style.backgroundColor = newColor; 
+    h1.innerText = newColor; 
+}); 
+
+const randomColor = () => {
+    function randomNumber() {
+        return Math.floor(Math.random() * 255); 
+    }
+    const r = randomNumber(); 
+    const g = randomNumber(); 
+    const b = randomNumber(); 
+    return `rgb(${r}, ${g}, ${b})`; 
+}
